53199385ac9dae4c729d75f339e69f86bb552364
jsp <zinsani@gmail.com>
2022-05-28 00:21:11 +0900

    feature: use restful API instead of downloaded json files.
    It requires '.env' file includes 'key' and 'token' data within which you
    can get from the Trello developer site.

--- index.js
+++ index.js
@@ -1,192 +1,20 @@
+require("dotenv").config();
+const axios = require("axios");
 const fs = require("fs");
 const jsonexport = require("jsonexport");
 
-function parseJsonToActions(jsonFile, offsetDay) {
-  const rawdata = fs.readFileSync(jsonFile);
-  const data = JSON.parse(rawdata);
-
-  const today = new Date();
-  const startDate = new Date(
-    today.getFullYear(),
-    today.getMonth(),
-    today.getDate() - offsetDay
-  );
-  const endDate = new Date(
-    today.getFullYear(),
-    today.getMonth(),
-    today.getDate() + 1
-  );
-  const todayDate = new Date(
-    today.getFullYear(),
-    today.getMonth(),
-    today.getDate()
-  );
-
-  function mapListTitleToActon(data) {
-    return action => {
-      const listId =
-        data.cards.find(c => c.id === action.data.card.id)?.idList ?? null;
-      const listName = listId
-        ? data.lists.find(l => l.id === listId).name
-        : `no list found (${action.data.card.name})`;
-      return { ...action, data: { ...action.data, list: listName } };
-    };
-  }
-
-  function mapChecklistProgress(data) {
-    return action => {
-      const checklists = data.checklists
-        .filter(c => c.name === "Main")
-        .find(c => c.idCard === action.data.card.id);
-      const checkItems = checklists ? checklists.checkItems : [];
-      const progress =
-        Math.round(
-          (checkItems.filter(c => c.state === "complete").length /
-            checkItems.length) *
-            100
-        ) / 100;
-      return { ...action, data: { ...action.data, progress } };
-    };
-  }
-
-  function filterForCheckCompletedItems(action) {
-    return (
-      action.type === "updateCheckItemStateOnCard" &&
-      action.data.checkItem.state === "complete"
-    );
-  }
-
-  function filterCommentCardItems(action) {
-    return action.type === "commentCard";
-  }
-
-  function populateCheckCompleted(action) {
-    return {
-      project: action.data.list,
-      member: action.memberCreator.fullName,
-      date: action.date,
-      item: action.data.card.name,
-      progress: action.data.progress,
-      done: "✓ " + action.data.checkItem.name,
-    };
-  }
-
-  function populateCommentCard(action) {
-    return {
-      project: action.data.list,
-      member: action.memberCreator.fullName,
-      item: action.data.card.name,
-      progress: action.data.progress,
-      date: action.date,
-      done: (action.data.text.startsWith("-") ? "" : "- ") + action.data.text,
-    };
-  }
-
-  function filterDateIn(startDate, endDate) {
-    return action => {
-      const d = new Date(action.date);
-      return d > startDate && d < endDate;
-    };
-  }
-
-  const groupBy = function (data, key) {
-    return data.reduce(function (carry, el) {
-      var group = el[key];
-
-      if (carry[group] === undefined) {
-        carry[group] = [];
-      }
-
-      carry[group].push(el);
-      return carry;
-    }, {});
+function filterDateIn(startDate, endDate) {
+  return action => {
+    const d = new Date(action.date);
+    return d > startDate && d < endDate;
   };
-
-  const actionsInThisWeek = data.actions
-    .filter(a => a.data.card)
-    .filter(filterDateIn(startDate, endDate))
-    .map(mapListTitleToActon(data))
-    .map(mapChecklistProgress(data));
-
-  const mergeChecklistWithCard = checklist => {
-    const card = data.cards.find(c => c.id === checklist.idCard);
-    const listName = card.idList
-      ? data.lists.find(l => l.id === card.idList).name
-      : `no list found (${card.name})`;
-
-    return {
-      project: listName,
-      member: card.idMembers
-        .map(idMember => data.members.find(m => m.id === idMember).fullName)
-        .join(", "),
-      item: card.name,
-      date: todayDate.toISOString(),
-    };
-  };
-
-  const todoList = data.checklists
-    .filter(cl => cl.name.toLowerCase().replace(" ", "").startsWith("todo"))
-    .map(cl => ({
-      ...mergeChecklistWithCard(cl),
-      todo: cl.checkItems
-        .filter(c => c.state !== "complete")
-        .filter(c => !c.name.startsWith("----"))
-        .map(c => ` ≥ ${c.name}`)
-        .join("\n"),
-    }))
-    .filter(({ todo }) => !!todo);
-
-  const actions = [
-    ...actionsInThisWeek
-      .filter(filterForCheckCompletedItems)
-      .map(populateCheckCompleted),
-    ...actionsInThisWeek
-      .filter(filterCommentCardItems)
-      .map(populateCommentCard),
-    ...todoList,
-  ]
-    .map(action => ({ ...action, date: formatDate(action.date) }))
-    .sort(sortByItemName);
-
-  const projects = groupBy(actions, "project");
-  const result = Object.keys(projects).reduce((acc, k) => {
-    let p = projects[k];
-    const itemGroup = groupBy(
-      projects[k].map(i => ({
-        ...i,
-        project: undefined,
-      })),
-      "item"
-    );
-
-    p = Object.keys(itemGroup).reduce((acc2, k2) => {
-      if (acc2[k2] === undefined)
-        acc2[k2] = {
-          progress: itemGroup[k2][0].progress,
-          member: itemGroup[k2][0].member,
-          done: [],
-        };
-
-      acc2[k2].done = itemGroup[k2].map(it => it.done);
-      return acc2;
-    }, {});
-
-    acc[k] = p;
-    return acc;
-  }, {});
-
-  console.table(
-    actions.map(action => ({ ...action, done: action.done?.substr(0, 40) }))
-  );
-
-  return actions;
 }
 
 function sortByItemName(a, b) {
-  if (a.item.toUpperCase() < b.item.toUpperCase()) {
+  if (a.item?.toLowerCase() < b.item?.toLowerCase()) {
     return -1;
   }
-  if (a.item.toUpperCase() > b.item.toUpperCase()) {
+  if (a.item?.toLowerCase() > b.item?.toLowerCase()) {
     return 1;
   }
   return 0;
@@ -212,10 +40,190 @@ const readline = require("readline").createInterface({
 });
 
 readline.question("Input day offset. (default: 7)", (offsetDate = "7") => {
-  const od = offsetDate ? Number(offsetDate) : 7;
-  const projects = parseJsonToActions("./projects.json", od);
-  const rnd = parseJsonToActions("./rnd.json", od);
-  writeJsonToCsv([...projects, ...rnd], "./output.csv");
+  const today = new Date();
+  const offsetDay = offsetDate ? Number(offsetDate) : 7;
+  const startDate = new Date(
+    today.getFullYear(),
+    today.getMonth(),
+    today.getDate() - offsetDay
+  );
+  const endDate = new Date(
+    today.getFullYear(),
+    today.getMonth(),
+    today.getDate() + 1
+  );
+  const todayDate = new Date(
+    today.getFullYear(),
+    today.getMonth(),
+    today.getDate()
+  );
+
+  const url = "https://api.trello.com/1";
+  const actionTypes = ["commentCard", "updateCheckItemStateOnCard"];
+  const key = process.env.key;
+  const token = process.env.token;
+  if (!key || !token) {
+    console.error("key and token have to be given.");
+    readline.close();
+    return;
+  }
+  const sufix = `key=${key}&token=${token}`;
+
+  axios
+    .get(`${url}/members/me/boards?fields=name,url&` + sufix)
+    .then(async res => {
+      console.log("all boards", res.data);
+
+      const boardData = res.data.filter(b =>
+        ["newmedia.projects", "newmedia.r&d"].includes(b.name.toLowerCase())
+      );
+
+      console.log("boardData ", boardData);
+
+      const allTodoLists = [];
+      const allActionLists = [];
+      for (const { name, id } of boardData) {
+        console.log("fetching all cards of board", name);
+        try {
+          const { data: allCards } = await axios.get(
+            `${url}/boards/${id}/cards?filter=open&${sufix}`
+          );
+          console.log("all cards count", allCards.length);
+          let todosPerCards = [];
+          for (const card of allCards) {
+            if (!card.idChecklists.length) continue;
+
+            const { data: checklists } = await axios.get(
+              `${url}/cards/${card.id}/checklists?${sufix}`
+            );
+            const matchingCheckLists = checklists.filter(cl =>
+              cl.name.toLowerCase().replace(" ", "").startsWith("todo")
+            );
+            if (!matchingCheckLists.length) continue;
+            if (
+              matchingCheckLists
+                .map(
+                  cl =>
+                    cl.checkItems.filter(ci => ci.state !== "complete").length >
+                    0
+                )
+                .filter(x => !!x).length === 0
+            )
+              continue;
+
+            const { data: list } = await axios.get(
+              `${url}/lists/${card.idList}?${sufix}`
+            );
+            const { data: members } = await axios.get(
+              `${url}/cards/${card.id}/members?${sufix}`
+            );
+
+            const todos = matchingCheckLists
+              .map(cl => ({
+                project: list.name,
+                item: card.name,
+                member: members.map(m => m.fullName).join(", "),
+                date: formatDate(startDate.toISOString()),
+                todo:
+                  `${cl.name.replace(/^\s?todo\s?/i, "\n")}` +
+                  cl.checkItems
+                    .filter(c => c.state !== "complete")
+                    .filter(c => !c.name.startsWith("---"))
+                    .filter(c => !c.name.startsWith("==="))
+                    .map(c => `→ ${c.name}`)
+                    .join("\n"),
+              }))
+              .filter(({ todo }) => !!todo);
+            todosPerCards = [
+              ...todosPerCards,
+              ...todos.filter(t => t.todo.length > 1),
+            ];
+          }
+          console.log("todosPerCards", todosPerCards);
+          allTodoLists.push({ board: name, data: todosPerCards });
+        } catch (e) {
+          /* handle error */
+          console.error(e);
+        }
+
+        try {
+          const { data: actionsFetched } = await axios.get(
+            `${url}/boards/${id}/actions?${sufix}`
+          );
+          const actionLists = [];
+          for (const action of (await actionsFetched)
+            .filter(filterDateIn(startDate, endDate))
+            .filter(a =>
+              a.type === "updateCheckItemStateOnCard"
+                ? a.data.checkItem.state === "complete"
+                : actionTypes.includes(a.type)
+            )) {
+            const card = action.data.card;
+            const { data: list } = await axios.get(
+              `${url}/cards/${card.id}/list?${sufix}`
+            );
+            const { data: checklist } = await axios.get(
+              `${url}/cards/${card.id}/checklists?${sufix}`
+            );
+            const { data: member } = await axios.get(
+              `${url}/actions/${action.id}/memberCreator?${sufix}`
+            );
+
+            const mainCheckList = checklist.find(c =>
+              c.name.toLowerCase().startsWith("main")
+            );
+
+            const progress = mainCheckList?.checkItems
+              ? Math.round(
+                  (mainCheckList.checkItems.filter(c => c.state === "complete")
+                    .length /
+                    mainCheckList.checkItems.length) *
+                    100
+                ) / 100
+              : undefined;
+
+            const done =
+              action.type === "commentCard"
+                ? action.data.text
+                : "✓ " + action.data.checkItem.name;
+
+            const d = {
+              project: list.name,
+              member: member.fullName,
+              item: card.name,
+              date: formatDate(action.date),
+              progress,
+              done,
+            };
+
+            actionLists.push(d);
+          }
+          allActionLists.push({ board: name, data: actionLists });
+        } catch (e) {
+          /* handle error */
+          console.error(e);
+          return null;
+        }
+      }
+
+      console.log("=============================");
+      console.log("allTodoLists", allTodoLists);
+      console.log("=============================");
+      console.log("allActionLists", allActionLists);
+      console.log("=============================");
+      return [allActionLists, allTodoLists];
+    })
+    .then(([actionList, todoList]) => {
+      const data = [
+        ...actionList.map(({ data }) => data).sort(sortByItemName),
+        ...todoList.map(({ data }) => data).sort(sortByItemName),
+      ];
+      console.log("writing data to output.csv");
+      writeJsonToCsv(data, "./output.csv");
+      console.log("completed!");
+      console.log("=============================");
+    })
+    .catch(console.error);
 
   readline.close();
 });
